dbc06d31af8af5c82ba577ac14044e3161f341cc
Janetta Olivia <103167367+olviajntta@users.noreply.github.com>
2023-04-24 23:14:32 +0700

    BahanMakanan

---
+++ Code/Objek/BahanMakanan.java
@@ -0,0 +1,24 @@
+public class BahanMakanan extends ObjectSim implements PurchasableObject{
+    private String nama;
+    private int price;
+    private int tingkatKenyang;
+
+    public BahanMakanan(String nama, int price, int kekenyangan){
+        super(nama);
+        this.price = price;
+        this.kekenyangan = kekenyangan;
+    } 
+
+    public String getNama(){
+        return nama;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public int getTingkatKenyang(){
+        return tingkatKenyang;
+    }
+
+}
