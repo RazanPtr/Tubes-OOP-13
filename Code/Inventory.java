4310153f9f4c9056b1b0066faa282e725ccd0ba5
RazanPtr <16521468@mahasiswa.itb.ac.id>
2023-04-24 19:37:37 +0700

    s

--- Code/Inventory.java
+++ Code/Inventory.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class Inventory<T> {
-    private Map<Object, Integer> items;
+    private Map<ObjectSim, Integer> items;
 
     public Inventory() {
         this.items = new HashMap<>();
     }
 
-    public void addItem(Object item, int quantity) {
+    public void addItem(ObjectSim item, int quantity) {
         if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity harus lebih besar dari 0");
         }
@@ -18,7 +18,7 @@ public class Inventory<T> {
         items.put(item, currQuantity + quantity);
     }
 
-    public void removeItem(Object item, int quantity) {
+    public void removeItem(ObjectSim item, int quantity) {
         if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity harus lebih besar dari 0");
         }
@@ -33,18 +33,18 @@ public class Inventory<T> {
         }
     }
 
-    public int getQuantity(Object item) {
+    public int getQuantity(ObjectSim item) {
         return items.getOrDefault(item, 0);
     }
 
-    public Set<Object> getItem() {
+    public Set<ObjectSim> getItem() {
         return items.keySet();
     }
 
     public void showInventory(){
         System.out.println("Inventory: ");
-        Set <Object> keys = items.keySet();
-        for (Object key : keys){
+        Set <ObjectSim> keys = items.keySet();
+        for (ObjectSim key : keys){
             int quantity = items.get(key);
             System.out.println(key + ": " + quantity);
         }
