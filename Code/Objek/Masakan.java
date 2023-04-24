765cf6d6bd719db551f5ddcc2959399eead790f0
Janetta Olivia <103167367+olviajntta@users.noreply.github.com>
2023-04-25 00:02:31 +0700

    masakan

---
+++ Code/Objek/Masakan.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Masakan {
+    private String nama;
+    private ArrayList<BahanMakanan> bahan;
+    private int tingkatKenyang;
+
+    public Masakan(String nama, ArrayList<BahanMakanan> bahan, int tingkatKenyang) {
+        super(nama);
+        this.bahan = bahan;
+        this.tingkatKenyang = tingkatKenyang;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public ArrayList<BahanMakanan> getBahan() {
+        return bahan;
+    }
+
+    public int getTingkatKenyang() {
+        return tingkatKenyang;
+    }
+}
