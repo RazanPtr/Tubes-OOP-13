ffb7accb4de2985e4ad7f10480713b0acef09dbf
RazanPtr <16521468@mahasiswa.itb.ac.id>
2023-04-26 12:04:12 +0700

    s

--- Code/Objek/ObjectSim.java
+++ Code/Objek/ObjectSim.java
@@ -1,10 +1,6 @@
 public abstract class ObjectSim {
 	private String nama;
-<<<<<<< HEAD
 	public ObjectSim(String nama) {
-=======
-	public ObjectSim (String nama) {
->>>>>>> e688c732f49ee1d171b8f8bfd658de93967ebc5b
 		this.nama = nama;
 	}
 	public String getNama() {
